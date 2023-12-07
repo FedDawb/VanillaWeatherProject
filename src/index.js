fbbbcc61a1ef6d2a09948faf4f0a5ab365de01b5
FedDawb <122037269+FedDawb@users.noreply.github.com>
2023-12-07 15:05:50 +0000

    Axios added to html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refeshWeather(response) {
+    console.log(response.data);
+}
+
+function searchCity(city) {
+    let apiKey = "35b0bf31b34b9e45428c221d0a92o09"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(refeshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value; 
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);   
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
